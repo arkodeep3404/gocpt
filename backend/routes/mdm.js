296ffda51d1d0faaf430bcbcaf54d901781874bd
ArkodeepC <arkodeep3404@gmail.com>
2024-02-29 15:07:39 +0530

    commit 2

---
+++ backend/routes/mdm.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const router = express.Router();
+const zod = require("zod");
+const jwt = require("jsonwebtoken");
+const { JWT_SECRET } = require("../config");
+const { authMiddleware } = require("../middleware/middleware");
