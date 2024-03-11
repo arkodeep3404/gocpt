778fb3d796559c8d1acb1e5b724ea56f5b247beb
Arkodeep Chatterjee <arkodeep3404@gmail.com>
2024-03-11 18:28:30 +0530

    added health check route and global error catch middleware

--- backend/routes/mdm.js
+++
@@ -1,6 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const zod = require("zod");
-const jwt = require("jsonwebtoken");
-const { JWT_SECRET } = require("../config");
-const { authMiddleware } = require("../middleware/middleware");
